55f80415a128eaac66ba0cf0af4b0b879d135c3f
Bryce Kerley <bkerley@brycekerley.net>
2016-06-19 19:31:21 -0400

    riak_post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  def riak_post
+    RiakPost.from_post self
+  end
 end
---
+++ app/models/riak_post.rb
@@ -0,0 +1,37 @@
+class RiakPost < Hashie::Mash
+  attr_accessor :robject
+
+  def self.find(id)
+    existing_blob = bucket.get id.to_s
+    new(existing_blob.data).tap do |created|
+      created.robject = existing_blob
+    end
+  end
+
+  def self.from_post(post)
+    existing_blob = bucket.get_or_new(post.id.to_s)
+    existing_data = existing_blob.data || {}
+    merged_data = existing_data.merge post.attributes
+    new(merged_data).tap do |created|
+      created.robject = existing_blob
+    end
+  end
+
+  def save
+    self.robject ||= bucket.get_or_new(id.to_s)
+    existing_data = robject.data || {}
+    merged = existing_data.merge self
+    robject.data = merged
+    robject.store && true
+  end
+
+  private
+
+  def self.bucket
+    client.bucket 'posts'
+  end
+
+  def self.client
+    RIAK_CLIENT
+  end
+end
