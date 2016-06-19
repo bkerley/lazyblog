6ad2eea636b5a4ed8ee538c9582bf1fa408f3a8d
Bryce Kerley <bkerley@brycekerley.net>
2016-06-19 12:50:39 -0400

    simple homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :users
+
+  root 'blog#index'
+
+  get 'p/:id' => 'blog#show', as: 'read'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
