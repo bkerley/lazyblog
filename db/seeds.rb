b485ec43a8e7465d90039faf43ca72ea7103dfd3
Bryce Kerley <bkerley@brycekerley.net>
2016-06-19 12:50:17 -0400

    add some posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = 10.times.map do |n|
+  User.create username: "user-#{n}", password_digest: "x"
+end
+
+10.times.each do |n|
+  p = Post.create name: "Post #{n}", body: n.to_s
+
+  rand(20).times do |c|
+    Comment.create post_id: p.id, user_id: users.sample, body: "asdf"
+  end
+end
